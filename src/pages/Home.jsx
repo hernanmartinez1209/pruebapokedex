1e76c1714c9b8ed3286dbe01c17076db80c32891
FredySanchezLopez <102775554+FredySanchezLopez@users.noreply.github.com>
2022-10-14 17:23:09 -0500

    tercer commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import FormHome from '../components/home/FormHome'
 
+
 const Home = () => {
   return (
     <article className='pokedex' >
       <img className='pokedex__img' src="./public/images/home/pokedex.png" alt="" />
-    <h1 className='pokedex__tilte'> pokedex</h1>
-    <h2 className='poquedex__subtitle'> hi trener</h2>
-    <p className='pokedex__text'>parrrafo parrafo</p>
-   <FormHome />
+      <h2 className='poquedex__subtitle'>Hi trainer</h2>
+      <p className='pokedex__text'>Give me your name to see the pokedex</p>
+      <FormHome />
     </article>
   )
 }
