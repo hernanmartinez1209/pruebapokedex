5b6785dde3d9f379897b1172250197a4dcdcf3d4
hernanmartinez1209 <108160168+hernanmartinez1209@users.noreply.github.com>
2022-10-19 02:08:06 -0600

    commit paso

--- src/components/PokedexById.jsx
+++ src/components/PokedexById.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import InputSearch from './pokedex/InputSearch'
 import Pokemon404 from './pokedex/Pokemon404'
-
+import stylesPOkedexBiId from './styles/stylesPOkedexBiId.css'
 const PokedexById = () => {
  const {id} =  useParams()
 
@@ -40,33 +40,36 @@ if (hasError) {
 console.log(pokemon);
 
   return (
-    <article>
-     <header>
-     <img src={pokemon?.sprites.other['official-artwork'].front_default} />
+    <article className='poke__card_principal' >
+     <header className={`poke__container_img ${pokemon?.types[0].type.name}`}>
+    
+     <img className='img__poke-by-id' src={pokemon?.sprites.other['official-artwork'].front_default} />
         
      </header>
      
-     <section>
-      <h2>{pokemon?.name}</h2>
-      <p>Height:<span>{pokemon?.height}</span></p>
-      <p>Weight:<span>{pokemon?.weight}</span></p>
+      <h2 className={`name__poke ${pokemon?.types[0].type.name} `}>{pokemon?.name}</h2>
+     <section className='info__poke-card' >
+      <div className='fuerza'>
+      <p className='height__poke' >Height:<span>{pokemon?.height}</span></p>
+      <p className='weight__poke'>Weight:<span>{pokemon?.weight}</span></p>
+      </div>
       <hr />
-      <p>#<span>{pokemon?.id}</span></p>
      </section>
+      <p className={`id__poke ${pokemon?.types[0].type.name} `} >#<span>{pokemon?.id}</span></p>
     
-     <section>
+     <section className='type__poke'>
        <h2>Type</h2>
        <ul className="card__poke_type">
           {pokemon?.types.map((type) => (
-            <li key={type.slot}>{type.type.name}</li>
+            <li className='list__type' key={type.slot}>{type.type.name}</li>
             ))}
             </ul>
     </section>
-     <section>
+     <section className='abilyts__poke'>
       <h2>Abilities</h2>
             <ul className="card__poke_abilitie">
             {pokemon?.abilities.map((abilitie) => (
-            <li key={abilitie.slot}>{abilitie.ability.name}</li>
+            <li className='list__abilitys' key={abilitie.slot}>{abilitie.ability.name}</li>
             ))}
             </ul>
      </section>
@@ -74,15 +77,21 @@ console.log(pokemon);
       <h2>stats</h2>
       <ul className="card__poke_stat">
             {pokemon?.stats.map((stat) => (
+            <div>
             <li key={stat.stat.url}>{stat.stat.name} <span>{stat.base_stat}</span></li>
+            <div className='barra'>
+               <img  className='centro' src='https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9uZG8lMjBkZSUyMGNvbG9yZXN8ZW58MHx8MHx8&w=1000&q=80' width={stat.base_stat} />
+              
+            </div>
+            </div>
             ))}
             </ul>
      </section>
-     <section>
-      <h2>Movements</h2>
+       <h2 className='title__move-poke' >Movements</h2>
+     <section className='moves__poke'>
      <ul className="card__poke_move">
             {pokemon?.moves.map((move) => (
-            <li key={move.slot}>{move.move.name}</li>
+            <li className='list__move' key={move.slot}>{move.move.name}</li>
             ))}
             </ul>
      </section>
